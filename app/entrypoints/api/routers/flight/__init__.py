8e2631b11cd8a470423990829d2830334d775d1b
quang181 <quangnm1@bizflycloud.vn>
2026-04-23 18:13:42 +0700

    [UPDATE]: add router

--- app/entrypoints/api/routers/flight/__init__.py
+++ app/entrypoints/api/routers/flight/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from app.entrypoints.api.routers.flight.offer.router import router as offer_router
 from app.entrypoints.api.routers.flight.list.router import router as list_router
 
 router = APIRouter(tags=["flight"])
 
 router.include_router(list_router)
+router.include_router(offer_router)
