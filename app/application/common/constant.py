ea103d3cbd6b88d9acc8b6a26b293063e4bd0de2
quang181 <quangnm1@bizflycloud.vn>
2026-04-23 17:59:04 +0700

    [UPDATE]: Update data mapping in constants

--- app/application/common/constant.py
+++ app/application/common/constant.py
@@ -1,2 +1,35 @@
 DATE_YYYY_MM_DD_REGEX = r"^\d{4}-\d{2}-\d{2}$"
 FLIGHT_SEARCH_CACHE_KEY_PREFIX = "flight_search"
+BOOKING_CACHE_KEY_PREFIX = "booking"
+UNKNOWN_LABEL = "Unknown"
+
+AIRLINE_LABELS = {
+    "MH": "Malaysia Airlines",
+    "AK": "AirAsia",
+    "SQ": "Singapore Airlines",
+}
+
+CABIN_LABELS = {
+    "Y": "Economy",
+    "W": "Premium Economy",
+    "J": "Business",
+    "F": "First",
+}
+
+PAYMENT_METHOD_LABELS = {
+    "CC": "Credit Card",
+    "DC": "Debit Card",
+    "BT": "Bank Transfer",
+}
+
+OFFER_STATUS_LABELS = {
+    "A": "Active",
+    "C": "Cancelled",
+    "H": "On Hold",
+    "R": "Requested",
+    "X": "Expired",
+}
+
+FARE_FAMILY_LABELS = {
+    "FL": "Premium",
+}
