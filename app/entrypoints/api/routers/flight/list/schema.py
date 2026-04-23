4f9317679d1aebc4d625e99c52d0ebb84fd04078
quang181 <quangnm1@bizflycloud.vn>
2026-04-23 18:10:46 +0700

    [UPDATE]: update list cabin allow

--- app/entrypoints/api/routers/flight/list/schema.py
+++ app/entrypoints/api/routers/flight/list/schema.py
@@ -11,6 +11,6 @@ class FlightListQuery(BaseModel):
     departure_date: str = Field(..., pattern=DATE_YYYY_MM_DD_REGEX)
     return_date: str = Field(..., pattern=DATE_YYYY_MM_DD_REGEX)
     pax_count: int
-    cabin: Literal["Y", "W", "C", "F"] = "Y"
+    cabin: Literal["Y", "W", "J", "F"] = "Y"
     page: int = Field(default=1, ge=1)
     page_size: int = Field(default=10, ge=1, le=100)
