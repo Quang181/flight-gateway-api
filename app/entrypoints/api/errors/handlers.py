9aa8f777d7c351ae2f4cef331fa3e21ca89319fd
quang181 <quangnm1@bizflycloud.vn>
2026-04-23 18:12:27 +0700

    [UPDATE]: support message error by lang for handle_http_exception

--- app/entrypoints/api/errors/handlers.py
+++ app/entrypoints/api/errors/handlers.py
@@ -3,18 +3,28 @@ from starlette.exceptions import HTTPException
 from fastapi import Request
 from starlette.responses import JSONResponse
 
-from app.entrypoints.api.errors.exceptions import AppHTTPException
+from app.entrypoints.api.errors.exceptions import AppHTTPException, LegacyApiException
 from app.entrypoints.api.errors.responses import error_response
 
 
 async def handle_http_exception(request: Request, exc: HTTPException) -> JSONResponse:
+    if isinstance(exc, LegacyApiException):
+        return error_response(
+            status_code=exc.status_code,
+            message=exc.message,
+            accept_language=request.headers.get("lang"),
+        )
+
     message = exc.detail if isinstance(exc.detail, str) else None
+    message_key = None
     if isinstance(exc, AppHTTPException):
         message = exc.message
+        message_key = exc.message_key
 
     return error_response(
         status_code=exc.status_code,
         message=message,
+        message_key=message_key,
         accept_language=request.headers.get("lang"),
     )
 
