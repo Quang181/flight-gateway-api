3f5615fab52e96b90f9211d27caa1ea762ccece3
quangnm <m.quang181201@gmail.com>
2026-04-24 10:30:22 +0700

    [UPDATE]: delete file

--- app/domain/ports/health_repository.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class HealthRepository(ABC):
-    @abstractmethod
-    async def ping(self) -> dict[str, str]:
-        """Return dependency health information."""
