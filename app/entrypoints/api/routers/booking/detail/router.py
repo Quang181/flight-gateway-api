1134392988af514624618662d16d63f1af3c83b6
quangnm <m.quang181201@gmail.com>
2026-04-24 10:40:06 +0700

    [UPDATE]:  update logic validate response

--- app/entrypoints/api/routers/booking/detail/router.py
+++ app/entrypoints/api/routers/booking/detail/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
 
 from app.application.use_cases.get_booking import GetBooking
-from app.entrypoints.api.decorators import require_token
 from app.entrypoints.api.dependencies import get_booking_use_case
 from app.entrypoints.api.routers.booking.detail.schema import BookingDetailResponse
 
@@ -14,4 +13,6 @@ async def get_booking(
     reference: str,
     use_case: GetBooking = Depends(get_booking_use_case),
 ) -> BookingDetailResponse:
-    return await use_case.execute(reference)
+    booking_detail = await use_case.execute(reference)
+    response = BookingDetailResponse(**booking_detail)
+    return response
