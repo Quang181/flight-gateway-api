090cda68ce418835aa4c82187e7bc00d302b0856
quangnm <m.quang181201@gmail.com>
2026-04-24 10:30:41 +0700

    [UPDATE]: delete file secure.py

--- app/entrypoints/api/routers/system/secure.py
+++
@@ -1,9 +0,0 @@
-from app.entrypoints.api.decorators import require_token
-from app.entrypoints.api.errors import AppAuthorizedResponse
-from app.entrypoints.api.routers.system import router
-
-
-@router.get("/secure/ping")
-@require_token
-async def secure_ping() -> None:
-    raise AppAuthorizedResponse()
