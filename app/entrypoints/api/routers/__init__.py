e90ee7d2d2a84586233f6bf8f700ef3159f49073
quang181 <quangnm1@bizflycloud.vn>
2026-04-24 17:39:36 +0700

    [UPDATE]: add prefix

--- app/entrypoints/api/routers/__init__.py
+++ app/entrypoints/api/routers/__init__.py
@@ -4,6 +4,6 @@ from app.entrypoints.api.routers.booking import router as booking_router
 from app.entrypoints.api.routers.flight import router as flight_router
 
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/flights")
 api_router.include_router(flight_router)
 api_router.include_router(booking_router)
